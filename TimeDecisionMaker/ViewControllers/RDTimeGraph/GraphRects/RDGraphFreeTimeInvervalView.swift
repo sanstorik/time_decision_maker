23ea25f8b999902f6c14720c48de7162333bbe54
sanstorik <shvetsovvitaliy06@gmail.com>
2019-05-20 17:58:45 +0300

    Updated free time view

--- TimeDecisionMaker/ViewControllers/RDTimeGraph/GraphRects/RDGraphFreeTimeInvervalView.swift
+++ TimeDecisionMaker/ViewControllers/RDTimeGraph/GraphRects/RDGraphFreeTimeInvervalView.swift
@@ -17,15 +17,33 @@ class RDGraphFreeIntervalView: RDGraphRect {
         return label
     }()
     
+    
+    private let addIV: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "add")?.withRenderingMode(.alwaysTemplate)
+        iv.tintColor = AppColors.alertSheetDarkButtonColor
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
 
     override func setupViews() {
         super.setupViews()
         backgroundColor = AppColors.freeIntervalDateColor
         addSubview(summaryLabel)
+        addSubview(addIV)
         
         let constraints = [
             summaryLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
-            summaryLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5)
+            summaryLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            
+            addIV.centerXAnchor.constraint(equalTo: centerXAnchor),
+            addIV.centerYAnchor.constraint(equalTo: centerYAnchor),
+            addIV.widthAnchor.constraint(lessThanOrEqualTo: heightAnchor, multiplier: 0.5),
+            addIV.widthAnchor.constraint(lessThanOrEqualToConstant: 40),
+            
+            addIV.heightAnchor.constraint(lessThanOrEqualTo: heightAnchor, multiplier: 0.5),
+            addIV.heightAnchor.constraint(lessThanOrEqualToConstant: 40)
         ]
         
         NSLayoutConstraint.activate(constraints)
